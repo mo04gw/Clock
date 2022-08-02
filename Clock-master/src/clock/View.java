984bfea4d4aa6091399af15348a5fd5ecfa89919
heidi <heidi@DESKTOP-1GO3JC8>
2022-08-02 12:58:32 +0100

    Added JPanels (with ClockPanel in the center) and a single button for setting an alarm

--- Clock-master/src/clock/View.java
+++ Clock-master/src/clock/View.java
@@ -8,31 +8,61 @@ import java.util.Observable;
 public class View implements Observer {
     
     ClockPanel panel;
+    //Container pane;
+
     
     public View(Model model) {
+        
         JFrame frame = new JFrame();
-        panel = new ClockPanel(model);
-        //frame.setContentPane(panel);
-        frame.setTitle("Java Clock");
+        
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Java Clock");
+        frame.setSize(600,500);
+        frame.setLayout(new BorderLayout());
+        frame.setVisible(true);
+        //JPanel Clock
+        panel = new ClockPanel(model);
+
+        JPanel panel_top= new JPanel();
+        JPanel panel_left = new JPanel();
+        JPanel panel_right = new JPanel();
+        JPanel panel_bottom = new JPanel();
+        JPanel panel_centre = panel;
+       
+        panel_top.setBackground(new Color(0x344765));
+        panel_left.setBackground(new Color(0x344765));
+        panel_right.setBackground(new Color(0x344765));
+        panel_bottom.setBackground(new Color(0x344765));
         
-        // Start of border layout code
+        panel_top.setPreferredSize(new Dimension(100, 50));
+        panel_left.setPreferredSize(new Dimension(100, 100));
+        panel_right.setPreferredSize(new Dimension(100, 100));
+        panel_bottom.setPreferredSize(new Dimension(100, 100));
+        panel_centre.setPreferredSize(new Dimension(200, 200));
         
-        // I've just put a single button in each of the border positions:
-        // PAGE_START (i.e. top), PAGE_END (bottom), LINE_START (left) and
-        // LINE_END (right). You can omit any of these, or replace the button
-        // with something else like a label or a menu bar. Or maybe you can
-        // figure out how to pack more than one thing into one of those
-        // positions. This is the very simplest border layout possible, just
-        // to help you get started.
+        frame.add(panel_top, BorderLayout.NORTH);
+        frame.add(panel_left, BorderLayout.WEST);
+        frame.add(panel_right, BorderLayout.EAST);
+        frame.add(panel_bottom, BorderLayout.SOUTH);
+        frame.add(panel_centre, BorderLayout.CENTER);
         
-        Container pane = frame.getContentPane();
         
+        //Buttons alarm
+        JButton buttonAdd = new JButton("Add Alarm");
+        panel_bottom.add(buttonAdd, BorderLayout.LINE_START);
+        
+        JButton buttonRemove = new JButton("Remove Alarm");
+        panel_bottom.add(buttonAdd, BorderLayout.LINE_END);
+        //frame.setContentPane(panel);
+        
+        //pane = frame.getContentPane();
+        //pane.add(panel, BorderLayout.CENTER);
+        //frame.setContentPane(panel);
+        //frame.setContentPane(panel);
+        /*
+        //Buttons
         JButton button = new JButton("Button 1 (PAGE_START)");
         pane.add(button, BorderLayout.PAGE_START);
-         
-        panel.setPreferredSize(new Dimension(200, 200));
-        pane.add(panel, BorderLayout.CENTER);
          
         button = new JButton("Button 3 (LINE_START)");
         pane.add(button, BorderLayout.LINE_START);
@@ -42,11 +72,9 @@ public class View implements Observer {
          
         button = new JButton("5 (LINE_END)");
         pane.add(button, BorderLayout.LINE_END);
-        
+        */
         // End of borderlayout code
-        
         frame.pack();
-        frame.setVisible(true);
     }
     
     public void update(Observable o, Object arg) {
